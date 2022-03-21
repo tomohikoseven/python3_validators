1d52ec7293e0c730017e12c16944763db7a46e40
tomohikoseven <tomohikoseven0905@gmail.com>
2022-03-21 10:40:21 +0900

    between 実装中.

--- tests/test_between.py
+++ tests/test_between.py
@@ -6,3 +6,14 @@ def test_success():
 
 def test_failure():
   assert not validators.between(2,3,5)
+
+@pytest.mark.parametrize(('value', 'min', 'max'), [(12, None, None)])
+def test_raises_assertion_error_for_invalid_args(value, min, max):
+  with pytest.raises(AssertionError):
+    assert validators.between(value, min=min, max=max)
+
+@pytest.mark.parametrize(('value','min','max'),[
+  (12,None,13),
+])
+def test_returns_true_on_valid_range(value, min, max):
+  assert validators.between(value, min=min, max=max)
--- validators/between.py
+++ validators/between.py
@@ -1,3 +1,11 @@
+from .extremes import Max, Min
 
 def between(value, min=None, max=None):
+  if min is None and max is None:
+    raise AssertionError('At least one of `min` or `max` must be specified.')
+
+  if min is None:
+    min = Min
+  if max is None:
+    max = Max
   return value >= min  and value <= max
